cdaa42f1f57627e169b69cbeb6b9247e991344f7
Kunal Shah <you@example.com>
2024-01-22 17:43:59 -0500

    completed code for db and config

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+//Configure sequelize library for our database
+
+const sequelize = require ('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+//allow us to use JAWSDB
+if (process.env.JAWSDB_URL) {
+    sequelize - new sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3001
+          }
+        );
+}
+
+//export sequelize for us to use 
+module.exports = sequelize;
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+-- drop any database with the name techBlog_db, and create a new database called techBlog_db
+DROP DATABASE IF EXISTS techBlog_db;
+CREATE DATABASE techBlog_db;
